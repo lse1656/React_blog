d2b6866b73677272c139bed42f64de3ba218ae29
lse1656 <lse1656@naver.com>
2021-07-19 13:49:17 +0900

    🔧 Install node-sass and Detail.scss에 스타일 적용

--- src/Detail.js
+++ src/Detail.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { useHistory, useParams } from 'react-router-dom'
 import styled from 'styled-components';
+import './Detail.scss';
 
 let 박스 = styled.div`
   padding : 20px;
@@ -22,8 +23,11 @@ function Detail(props){
   return(
     <div className="container">
       <박스>
-        <제목 색상={'blue '}>Detail</제목>
+        <제목 className="red">Detail</제목>
       </박스>
+      <div className="my-alert2">
+        <p>재고가 얼마 남지 않았습니다!</p>
+      </div>
       <div className="row">
         <div className="col-md-6">
           <img src="https://codingapple1.github.io/shop/shoes1.jpg"/>
